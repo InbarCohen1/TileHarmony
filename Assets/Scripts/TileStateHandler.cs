8573a982f03f413e6ee7db07628dd83b85105acc
InbarCohen1 <inbar74587@gmail.com>
2024-09-17 18:14:31 +0300

    added TilesTypes

---
+++ Assets/Scripts/TileStateHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tile State")]
+
+public class TileStateHandler : ScriptableObject
+{
+    public Color _backgroundColor;
+    public Color _textColor;
+}
