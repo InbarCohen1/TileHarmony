0f4924195873222ce4cb50a039aaed5de54bee74
InbarCohen1 <inbar74587@gmail.com>
2024-10-06 11:39:30 +0300

    Added "GameState" class

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameState
+{
+    public List<Vector2Int> TilePositions { get; }
+    public List<int> TileValues { get; }
+    public int Score { get; }
+
+    public GameState(List<Vector2Int> positions, List<int> values, int currentScore)
+    {
+        TilePositions = new List<Vector2Int>(positions);
+        TileValues = new List<int>(values);
+        Score = currentScore;
+    }
+}
